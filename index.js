434760349cd266afd9d33a2035ae1a7e68c623ad
Waylon Flinn <waylonflinn@gmail.com>
2016-01-09 16:06:52 -0600

    pad input rows during texture creation
    this removes the creation of a duplicate set of input data
    when padding to a multiple of four. data is uploaded in steps
    using texSubImage2D instead.

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ module.exports = {
     "test" : test
 };
 
-var CHANNELS_PER_TEXEL = 4;
+// RGBA is the standard input/ouput texture
+var COMPONENTS_PER_TEXEL = 4;
 
 /* Wrap the GL calculation object in a (relatively) user friendly function that
     accepts TypedArrays
@@ -31,21 +32,24 @@ var CHANNELS_PER_TEXEL = 4;
 function sgemm(M, N, K, alpha, A, B, beta, C){
 
     // pack each matrix into a single RGBA texel array, with the second transposed
-    var texels0 = packData(M, K, A, false),
-    	texels1 = packData(K, N, B, true);
+    var texels0 = A,
+    	texels1;
 
-    var mod = (K % CHANNELS_PER_TEXEL),
-    	pad = mod == 0 ? 0 : CHANNELS_PER_TEXEL - mod;
+    var rem = (K % COMPONENTS_PER_TEXEL),
+    	pad = rem == 0 ? 0 : COMPONENTS_PER_TEXEL - rem;
+
+    texels1 = transpose(K, N, B);
 
     // create input textures from data
-    var texture0 = gl.createInputTexture(M, K + pad, texels0);
-    var texture1 = gl.createInputTexture(N, K + pad, texels1);
+    var texture0 = gl.createInputTexture(M, K, texels0);
+    var texture1 = gl.createInputTexture(N, K, texels1);
 
     var destTexture = gl.createDestinationTexture(M, N);
 
     sgemmcalculator.calculate(M, N, K + pad, alpha, texture0, texture1, null, destTexture);
 
-    return new Float32Array(gl.readData(M, N));
+    rawBuffer = gl.readData(M, N);
+    return new Float32Array(rawBuffer);
 
 }
 
@@ -53,26 +57,23 @@ function saxpy(N, a, X, Y){
 
     var rawBuffer;
 
-	var mod = (N % CHANNELS_PER_TEXEL),
-		pad = mod == 0 ? 0 : CHANNELS_PER_TEXEL - mod;
+	var mod = (N % COMPONENTS_PER_TEXEL),
+		pad = mod == 0 ? 0 : COMPONENTS_PER_TEXEL - mod;
 
-    // replace with ArrayBuffer.transfer, when available
-	var texels0,// = new Float32Array(N + pad);//packData(1, N, X, false),
-		texels1;// = new Float32Array(N + pad);//packData(1, N, Y, false);
+	var texels0 = X,
+		texels1;
 
-
-    if(pad == 0){
-        texels0 = X;
+    // TODO: special shader for constant Y
+    if(isFloat32Array(Y)){
+        texels1 = Y;
     } else {
-        texels0 = new Float32Array(N + pad);
-        texels0.set(X);
+        texels1 = new Float32Array(N);
+        texels1.fill(Y);
     }
-    texels1 = new Float32Array(N + pad);
-    texels1.fill(Y);
 
     // create input textures from data
-    var texture0 = gl.createInputTexture(1, N + pad, texels0);
-    var texture1 = gl.createInputTexture(1, N + pad, texels1);
+    var texture0 = gl.createInputTexture(1, N, texels0);
+    var texture1 = gl.createInputTexture(1, N, texels1);
 
     var destTexture = gl.createDestinationTexture(1, N + pad);
 
@@ -88,79 +89,6 @@ function saxpy(N, a, X, Y){
     run floatdecode shader
     return extracted result
  */
-/* Pack the given matrix data into texel layout for use by texture shader.
-
-   This layout places consecutive elements of the input data into separate
-   channels, padding to a multiple of four (with zeros) where necessary to fill
-   out the final RGBA texel in a column.
-
-   r - row count
-   c - column count
-   data - TypedArray containing matrix data
-   transpose - whether or not to transpose the data when packing
- */
-function packData(r, c, data, transpose) {
-
-	var CHANNELS_PER_TEXEL = 4; // RGBA: four channels, one per color
-
-	var k = !transpose ? c : r,
-        isArray = isFloat32Array(data);
-
-	var mod = (k % CHANNELS_PER_TEXEL),
-		pad = mod == 0 ? 0 : CHANNELS_PER_TEXEL - mod;
-
-	if (mod === 0 && !transpose && isArray) {
-		// special case if column count is a multiple of number of channels
-		return data;
-	}
-
-	// dimensions
-	var i, j, p;
-
-	var texelcount = !transpose ? r*(c + pad) : c*(r + pad);
-
-	// create Float32Array to hold padded texel data
-	var texels = new Float32Array(texelcount);
-
-	for(i = 0; i < r; i++){
-
-		if(!transpose){
-
-            if(isArray){
-    			// copy actual data
-    			for(j = 0; j < c; j++){
-    				texels[i * (c + pad) + j] = data[i * c + j];
-    			}
-            } else {
-    			// assume it's a constant
-    			for(j = 0; j < c; j++){
-    				texels[i * (c + pad) + j] = data;
-    			}
-            }
-
-			// pad last texel in this row with zeros
-			for(p = 0; p < pad; p++){
-				texels[i * (c + pad) + j + p] = 0.0;
-			}
-		} else {
-
-			// copy actual data, transposed
-			for(j = 0; j < c; j++){
-				texels[j * (r + pad) + i] = data[i * c + j];
-			}
-
-			// pad last texel in this row with zeros
-			for(p = 0; p < pad; p++){
-				texels[j * (r + pad) + i + p] = 0.0;
-			}
-
-		}
-
-	}
-
-	return texels;
-};
-
 function isFloat32Array(obj) { return Object.prototype.toString.call(obj) === "[object Float32Array]"; }
 
 
