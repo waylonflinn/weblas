6321bf7e5f2cb3a02b6fb4d870c07e2351fb8e7f
Waylon Flinn <waylonflinn@gmail.com>
2016-01-25 16:57:22 -0600

    whitespace

--- lib/glsl/sclmp/standalone.glsl
+++ lib/glsl/sclmp/standalone.glsl
@@ -16,7 +16,7 @@ void main(void) {
 	// get the implied row and column from .y and .x of passed (output)
 	// texture coordinate. These map directly to input texture space when
 	// the relevant dimensions are the same.
-	 float row = outTex.y;
+	float row = outTex.y;
 	float col = outTex.x;
 
 	// return 0.0 if in padded region of output texture
@@ -28,8 +28,9 @@ void main(void) {
 	// direct usage of col requires output be padded exactly like input
 	vec4 x = texture2D( X, vec2(col, row));
 	vec4 val = clamp(x, a, b);
+
 	// select and output channel (standalone version only)
-	int channel = int(mod(col * float(N + pad), 4.0 ));
+	int channel = int(mod(col * float(N + pad), 4.0));
 	float sum = select_index(val, channel);
 
 	if (sum == 0.) {
@@ -37,6 +38,6 @@ void main(void) {
 		return;
 	}
 
-	 // output vec4 with bytes for an IEEE754 32-bit floating point number
+	// output vec4 with bytes for an IEEE754 32-bit floating point number
 	gl_FragColor = encode_float(sum);
 }
