718e3dc25731751c05da9c1b3b0cab7059c71e89
Waylon Flinn <waylonflinn@gmail.com>
2016-02-10 10:56:58 -0600

    generalize submatrix shader to partial pixel offset

--- lib/glsl/submatrix.glsl
+++ lib/glsl/submatrix.glsl
@@ -21,8 +21,29 @@ void main(void) {
 	float col_t = outTex.x;
 	float col = (col_t * (N + pad) - 2.0); // index of first element in pixel (output matrix space)
 
-	// direct usage of col requires output be padded exactly like input
-	vec4 x = texture2D( X, vec2((col + 2.0 + offset) / (N_in + pad_in) , row_t));
+	vec4 x;
+	float pixel_part = mod(offset, 4.0);
+	// are we in the middle of an input pixel?
+	if(offset == 0.0 || pixel_part == 0.0){
+		// no, take the whole thing
+		x = texture2D( X, vec2((col + 2.0 + offset - pixel_part) / (N_in + pad_in) , row_t));
+	} else {
+		// yes, straddle pixels
+		vec4 x0, x1;
+		x0 = texture2D( X, vec2((col + 2.0 + offset - pixel_part) / (N_in + pad_in) , row_t));
+		x1 = texture2D( X, vec2((col + 6.0 + offset - pixel_part) / (N_in + pad_in) , row_t));
+
+		if(pixel_part == 1.0){
+			x.rgb = x0.gba;
+			x.a = x1.r;
+		} else if(pixel_part == 2.0){
+			x.rg = x0.ba;
+			x.ba = x1.rg;
+		} else if(pixel_part == 3.0){
+			x.r = x0.a;
+			x.gba = x1.rgb;
+		}
+	}
 
 	// fix padded region
 	if(pad > 0.0 && col + 4.0 > N ) {
