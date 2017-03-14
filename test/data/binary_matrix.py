5fbcd05060e520a53e7ea6e4cb2e02a48e228602
Waylon Flinn <waylonflinn@gmail.com>
2017-03-14 11:46:29 -0500

    allow for test data types other than float32

--- test/data/binary_matrix.py
+++ test/data/binary_matrix.py
@@ -5,18 +5,40 @@ to JSON files.
 """
 import json
 import numpy as np
+import os
 
 
-def read(path):
+type_map = {
+	'.i32' : np.int32,
+	'.u32' : np.uint32,
+	'.f32' : np.float32,
+	'.i64' : np.int64,
+	'.u64' : np.uint64,
+	'.f64' : np.float64
+}
+
+def get_extension(path):
+	filename, file_extension = os.path.splitext(path)
+	return file_extension
+
+def read(path, dtype=np.float32):
+
+	extension = get_extension(path)
+	if extension in type_map:
+		dtype = type_map[extension]
 
 	with open(path, 'rb') as f:
-		matrix = np.fromfile(f, dtype=np.float32)
+		matrix = np.fromfile(f, dtype=dtype)
 
 	return matrix
 
-def write(path, matrix):
+def write(path, matrix, dtype=np.float32):
+
+	extension = get_extension(path)
+	if extension in type_map:
+		dtype = type_map[extension]
 
 	with open(path, 'wb') as f:
-		f.write(matrix.astype(np.float32).tostring())
+		f.write(matrix.astype(dtype=dtype).tostring())
 
 	return matrix
